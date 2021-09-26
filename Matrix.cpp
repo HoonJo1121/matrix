29b5e3aaf8d612ae33b11d4cf7814dcfb9db4792
Hoon Jo <to.jhoon@gmail.com>
2021-09-26 22:30:24 +0900

    Add matrix data storage

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,9 +1,28 @@
 #include "Matrix.h"
 
-Matrix::Matrix()
+Matrix::Matrix(uint32_t rows, uint32_t cols)
+	: rows_(rows)
+	, cols_(cols)
+	, data_(NULL)
 {
+	data_ = new double[rows_ * cols_];
+	memset(data_, 0, rows_ * cols_ * sizeof(double));
 }
 
 Matrix::~Matrix()
 {
+	if (data_ != NULL)
+	{
+		delete[] data_;
+	}
+}
+
+uint32_t Matrix::getRows()
+{
+	return rows_;
+}
+
+uint32_t Matrix::getCols()
+{
+	return cols_;
 }
--- Matrix.h
+++ Matrix.h
@@ -1,9 +1,24 @@
 #pragma once
+
+#include <cstdint>
+#include <cstring>
+
 class Matrix
 {
 public:
-	Matrix();
+	Matrix(uint32_t rows, uint32_t cols);
 	~Matrix();
 
+	uint32_t getRows();
+	uint32_t getCols();
+
+	inline double& at(uint32_t row, uint32_t col)
+	{
+		return data_[row * cols_ + col];
+	};
+
 private:
+	uint32_t rows_;
+	uint32_t cols_;
+	double* data_;
 };
